1c3d571b7e425064b9f51cbe19036c7b1f3d982a
Rsantct <rsantct@gmail.com>
2019-12-23 20:33:23 +0100

    Add files via upload

---
+++ ecapre_control.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+"""
+    ecapre's system control
+
+    Usage:
+
+        ecapre_control.py [command value] [add]
+
+        Commands:
+
+        level       xx (dB)
+        balance     xx (dB)
+        loudness    on | off |toggle
+        bass        xx (dB)
+        treble      xx (dB)
+        target      room-house (dB-dB)
+
+        Use 'add' for relative adjustment
+"""
+from math import log10
+import yaml
+import sys
+from os.path import expanduser
+
+UHOME=expanduser('~')
+sys.path.append( f'{UHOME}/ecapre/share' )
+
+from   share.ecanet        import ecanet
+import share.eca_mbeq_ctrl as mbeq
+import share.eca_Eq4p_ctrl as Eq4p
+
+with open('ecapre.config', 'r') as f:
+    cfg = yaml.load(f)
+
+HEADROOM         =  cfg['headroom']
+MIN_LOUD_COMPENS =  cfg['min_loud_compens']
+MAX_LOUD_COMPENS =  cfg['max_loud_compens']
+TONE_SPAN        =  cfg['tone_span']
+REF_SPL_GAIN     =  cfg['ref_spl_gain']
+
+def isFloat(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def set_level(chain, dB, balance):
+    """ This is for the -eadb preset as the first chain operator.
+        It works with dB values
+    """
+    ch_value = dB + REF_SPL_GAIN - HEADROOM + balance/2.0 * {'L':-1, 'R':1}[chain]
+    cmds = [ f'c-select {chain}', f'cop-set 1,1,{str(ch_value)}' ]
+    for cmd in cmds:
+        ecanet(cmd)
+
+def get_level(chain):
+    """ This is for the -eadb preset as the first chain operator.
+        It works with dB values
+    """
+    level = None
+    cmd = f'c-select {chain}'
+    ecanet(cmd)
+    cmd = 'cop-get 1,1'
+    tmp = ecanet(cmd).split('\r\n')
+    level = float(tmp[1])
+    return level + HEADROOM
+
+def read_command_line():
+
+    cmd, arg, relative = None, None, False
+
+    opcs = sys.argv[1:]
+
+    if 'add' in opcs:
+        relative = True
+        opcs.remove('add')
+
+    try:
+        cmd = opcs[0]
+    except:
+        raise
+
+    try:
+        arg = opcs[1]
+    except:
+        pass
+
+    return cmd, arg, relative
+
+def print_state():
+
+    with open(state_fname, 'r') as f:
+        state = yaml.load(f)
+
+    line1 = 'Vol:  vvvv  Bal: bbbb  Loud: lll'
+    line2 = 'Bass: ssss  Treb: tttt '
+
+    line1 = line1.replace('vvvv', str(state['level']).ljust(5))
+    line1 = line1.replace('bbbb', str(state['balance']).ljust(5))
+    line1 = line1.replace('lll', {True:'on', False:'off'}[ state['loudness_track'] ] )
+    line2 = line2.replace('ssss', str(state['bass']).ljust(5))
+    line2 = line2.replace('tttt', str(state['treble']).ljust(5))
+
+    print(line1)
+    print(line2)
+
+def restore():
+    """ restore last settings from disk file .state.yml
+    """
+    for chain in ('L','R'):
+        print( f'(ecapre_control) restoring [{chain}] from disk file .state.yml' )
+        # Level
+        set_level( chain, state['level'], state['balance'] )
+
+        # Setting Loudness level compensation, by following
+        # the attenuation dB below level=0dB (reference SPL)
+        loud_level = max( min( -state['level'], MAX_LOUD_COMPENS), MIN_LOUD_COMPENS)
+        mbeq.apply_loudness( loud_level )
+
+        # Loudness_track
+        mbeq.mbeq_bypass( {True:'off', False:'on'} [ state['loudness_track'] ] )
+
+        # Tone
+        Eq4p.set_tone('bass',   state['bass'] )
+        Eq4p.set_tone('treble', state['treble'] )
+
+if __name__ == '__main__':
+
+    state_fname = '.state.yml'
+
+    with open(state_fname, 'r') as f:
+        state = yaml.load(f)
+
+    if sys.argv[1:]:
+
+        cmd, arg, relative = read_command_line()
+
+        # Level adjustment
+        if cmd == 'level' and isFloat(arg):
+
+            if relative:
+                level = state['level'] + float(arg)
+            else:
+                level = float(arg)
+
+            for chain in 'L','R':
+                set_level(chain, level, state['balance'])
+
+            # Setting Loudness level compensation, by following
+            # the attenuation dB below level=0dB (reference SPL)
+            loud_level = max( min( -level, MAX_LOUD_COMPENS), MIN_LOUD_COMPENS)
+            mbeq.apply_loudness( loud_level )
+            state['level'] = level
+
+        # Balance
+        elif cmd in ('balance', 'bal'):
+            bal = float(arg)
+            for chain in 'L','R':
+                set_level(chain, state['level'], bal)
+            state['balance'] == bal
+
+        # Loudness_track management
+        elif cmd in ('loud', 'loudness'):
+            if 'on' in arg:
+                mbeq.mbeq_bypass('off')
+                state['loudness_track'] = True
+            if 'off' in arg:
+                mbeq.mbeq_bypass('on')
+                state['loudness_track'] = False
+
+        # Loudness reference management
+        # ** WIP **
+
+        # Bass setting
+        elif cmd == 'bass':
+            dB = float(arg)
+            # clamp
+            dB = max(min(dB, TONE_SPAN), -TONE_SPAN)
+            Eq4p.set_tone('bass', dB, relative )
+            state['bass'] = dB
+
+        # Treble setting
+        elif cmd == 'treble':
+            dB = float(arg)
+            # clamp
+            dB = max(min(dB, TONE_SPAN), -TONE_SPAN)
+            Eq4p.set_tone('treble', dB, relative )
+            state['treble'] = dB
+
+        # Target EQ
+        elif cmd == 'target':
+            room  = float(arg.split('-')[0])
+            house = float(arg.split('-')[1])
+            Eq4p.set_target(room, house )
+            state['target'] = f'{str(round(room,1))} {str(round(house,1))}'
+
+        # Restore last settings from disk file .state.yml
+        elif cmd == 'restore':
+            restore()
+
+        # Help
+        elif '-h' in cmd:
+            print(__doc__)
+
+        # Saving the new settings
+        with open(state_fname, 'w') as f:
+            yaml.dump( state, f, default_flow_style=False )
+
+    # If no arguments, print the current settings
+    else:
+        print_state()
+
---
+++ ecapre_start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "(!) REMEMBER TO USE THE CURRENT SAMPLE RATE WHEN STARTING JACKD"
+FS=44100
+
+MYDIR=$(dirname "$0")
+
+# This is for custom installing LADSPA plugins on this directory
+export LADSPA_PATH=$MYDIR/lib/ladspa
+
+echo "(i) LADSPA_PATH=""$LADSPA_PATH"
+
+# killing any ecasound an jack
+killall -KILL ecasound
+killall -KILL JackBridge
+killall -KILL jackdmp
+
+# Exit if just want to stop
+if [[ $1 == 'stop' ]]; then
+    echo "Stopped: Ecasound, JackBridge, Jack."
+    exit 0
+fi
+sleep 3
+
+# Jack
+/usr/local/bin/./jackdmp -R -d coreaudio -r "$FS" -p 128 -o 2 -i 2 \
+-C AppleHDAEngineInput:1B,0,1,0:1 -P AppleHDAEngineOutput:1B,0,1,1:0 &
+sleep 3
+
+# JackRouter
+/Users/rafaelsanchez/Downloads/JackBridge/JackBridge &
+sleep .5
+
+# Ecasound
+ecasound  --server  -s:"${HOME}"/ecapre/share/eq/ecapre.ecs  1>/dev/null 2>&1 &
+sleep 3
+
+
+# Loading Eq4p plugin defaults (tones and target bands)
+"${HOME}"/ecapre/share/eca_Eq4p_ctrl.py \
+          "${HOME}"/ecapre/share/eq/Eq4p_default.yml
+
+
+# Restoring ecapre status
+"${HOME}"/ecapre/ecapre_control.py restore
+
+# Wiring:
+
+#jack_connect  'system:capture_1'  'ecasound:in_1'  1>/dev/null 2>&1
+#jack_connect  'system:capture_2'  'ecasound:in_2'  1>/dev/null 2>&1
+
+jack_connect  'ecasound:out_1'  'system:playback_1'  1>/dev/null 2>&1
+jack_connect  'ecasound:out_2'  'system:playback_2'  1>/dev/null 2>&1
+
+jack_connect  'JackBridge #1:output_0'  'ecasound:in_1'  1>/dev/null 2>&1
+jack_connect  'JackBridge #1:output_1'  'ecasound:in_2'  1>/dev/null 2>&1
