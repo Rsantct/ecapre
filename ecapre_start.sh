13b3f0f0a8922d86feda1ceb89cafcb9584b1f9f
Rsantct <rsantct@gmail.com>
2019-12-24 11:07:39 +0100

    Update ecapre_start.sh

--- ecapre_start.sh
+++ ecapre_start.sh
@@ -9,10 +9,11 @@ export LADSPA_PATH=$LADSPA_PATH:"${HOME}"/ecapre/lib/ladspa
 
 echo "(i) LADSPA_PATH=""$LADSPA_PATH"
 
-# killing any ecasound an jack
+# killing any ecasound an jack stuff
 killall -KILL ecasound
 killall -KILL JackBridge
 killall -KILL jackdmp
+killall -KILL qjackctl
 
 # Exit if just want to stop
 if [[ $1 == 'stop' ]]; then
@@ -26,9 +27,17 @@ sleep 3
 -C AppleHDAEngineInput:1B,0,1,0:1 -P AppleHDAEngineOutput:1B,0,1,1:0 &
 sleep 3
 
-# JackRouter
+# Qjackctl (optional just to check if things runs well)
+#/Applications/Jack/qjackctl.app/Contents/MacOS/qjackctl &
+
+# JackBridge (formerly JackRouter)
 "${HOME}"/bin/JackBridge &
 sleep .5
+
+# Setting JackBridge as the default system's sound device
+# https://github.com/deweller/switchaudio-osx
+SwitchAudioSource -s 'JackBridge'
+
 
 # Ecasound
 ecasound  --server  -s:"${HOME}"/ecapre/share/eq/ecapre.ecs  1>/dev/null 2>&1 &
@@ -53,3 +62,5 @@ jack_connect  'ecasound:out_2'  'system:playback_2'  1>/dev/null 2>&1
 
 jack_connect  'JackBridge #1:output_0'  'ecasound:in_1'  1>/dev/null 2>&1
 jack_connect  'JackBridge #1:output_1'  'ecasound:in_2'  1>/dev/null 2>&1
+
+
