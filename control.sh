7821bb0417938098a4dbf438aef8f388bae6720f
Rsantct <rsantct@gmail.com>
2020-11-29 01:09:43 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-THIS_PATH="`dirname \"$0\"`"                # relative
-THIS_PATH="`( cd \"$THIS_PATH\" && pwd )`"  # absolutized and normalized
-"$THIS_PATH"/share/services/ecapre_control.py $1
+"$HOME"/ecapre/share/services/ecapre_control.py $1 $2
 
